950854f22149d828c2cf503ded9f89ea0b533470
Caio <caiovictorarantes@gmai.com>
2026-06-07 14:10:55 -0300

    isercao de header do formulário de cadastro de transação.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import NavigationRoutes from '@/routes'
 import'./src/styles/global.css'
 
 import {SnackBar }from'@/components/SnackBar'
 import {AuthContextProvider }from'@/context/auth.context'
+import {BottomSheetProvider }from'@/context/bottom-sheet.context'
 import {SnackbarContextProvider }from'@/context/snackbar.context'
-
+import NavigationRoutes from '@/routes'
+import {GestureHandlerRootView }from'react-native-gesture-handler'
 
 export default function App() {
 return (
+<GestureHandlerRootView className="flex-1">
 <SnackbarContextProvider>
 <AuthContextProvider>
+<BottomSheetProvider>
 <NavigationRoutes/>
 
 <SnackBar/>
+</BottomSheetProvider>
 </AuthContextProvider>
 </SnackbarContextProvider>
+</GestureHandlerRootView>
   )
 }
--- src/routes/PrivateRoutes/index.tsx
+++ src/routes/PrivateRoutes/index.tsx
@@ -1,16 +1,16 @@
-import { Home } from '@/screens/Home'
-import { createStackNavigator } from '@react-navigation/stack'
+import {Home }from'@/screens/Home'
+import {createStackNavigator }from'@react-navigation/stack'
 
-export type PrivateStackParamsList = {
-  Home: undefined
+export type PrivateStackParamsList= {
+  Home:undefined
 }
 
-export const PrivateRoutes = () => {
-  const PrivateStack = createStackNavigator<PrivateStackParamsList>()
+export const PrivateRoutes= () => {
+const PrivateStack=createStackNavigator<PrivateStackParamsList>()
 
-  return (
-    <PrivateStack.Navigator>
-      <PrivateStack.Screen name="Home" component={Home} />
-    </PrivateStack.Navigator>
+return (
+<PrivateStack.Navigator screenOptions={{ headerShown:false }}>
+<PrivateStack.Screen name="Home" component={Home}/>
+</PrivateStack.Navigator>
   )
 }
