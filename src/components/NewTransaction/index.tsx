16a156a99aff13838285d7b6456a76355f6d6504
Caio <caiovictorarantes@gmai.com>
2026-06-07 14:44:51 -0300

    feat: implementar validação de transações e criar contexto para gerenciamento

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,79 +1,146 @@
-import {SelectCategoryModal }from'@/components/SelectCategoryModal'
-import {TransactionTypeSelector }from'@/components/TransactionTypeSelector'
-import {useBottomSheetContext }from'@/context/bottom-sheet.context'
-import {colors }from'@/shared/colors'
-import {CreateTransactionRequest }from'@/shared/interfaces/http/createTransactionRequest'
-import {MaterialIcons }from'@expo/vector-icons'
-import {useState }from'react'
-import {Text,TextInput,TouchableOpacity,View }from'react-native'
-import CurrencyInput from'react-native-currency-input'
+import { AppButton } from '@/components/AppButton'
+import { ErrorMessage } from '@/components/ErrorMessage'
+import { SelectCategoryModal } from '@/components/SelectCategoryModal'
+import { TransactionTypeSelector } from '@/components/TransactionTypeSelector'
+import { useBottomSheetContext } from '@/context/bottom-sheet.context'
+import {useTransactionContext }from'@/context/transaction.context'
+import { colors } from '@/shared/colors'
+import {useErrorHandler }from'@/shared/hooks/useErrorHandler'
+import { CreateTransactionRequest } from '@/shared/interfaces/http/createTransactionRequest'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useState } from 'react'
+import { ActivityIndicator, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import CurrencyInput from 'react-native-currency-input'
+import * as yup from 'yup'
+import { transactionSchema } from './schema'
 
-export const NewTransaction= () => {
-const { closeBottomSheet }=useBottomSheetContext()
+type ValidationErrorsTypes = Record<
+  keyof CreateTransactionRequest,
+  string
+>
 
-const [transaction,setTransaction]=useState<CreateTransactionRequest>({
-    description:'',
-    typeId:0,
-    categoryId:0,
-    value:0,
+export const NewTransaction = () => {
+  const { closeBottomSheet } = useBottomSheetContext()
+  const { createTransaction }=useTransactionContext()
+  const { errorHandler }=useErrorHandler()
+  const [transaction, setTransaction] = useState<CreateTransactionRequest>({
+    description: '',
+    typeId: 0,
+    categoryId: 0,
+    value: 0,
   })
+  const [loading,setLoading]=useState(false)
+  const [validationErrors, setValidationErrors] =
+    useState<ValidationErrorsTypes>({} as ValidationErrorsTypes)
 
-const setTransactionData= (
-key:keyof CreateTransactionRequest,
-value:string|number
+  const setTransactionData = (
+    key: keyof CreateTransactionRequest,
+    value: string | number
   ) => {
-setTransaction((prevData) => ({
+    setTransaction((prevData) => ({
       ...prevData,
-      [key]:value,
+      [key]: value,
     }))
   }
 
-return (
-<View className="px-8">
-<View className="w-full flex-row items-center justify-between">
-<Text className="text-white text-xl font-bold">
+const handleCreateTransaction = async () => {
+  try {
+    setLoading(true)
+
+    await transactionSchema.validate(transaction, {
+      abortEarly: false,
+    })
+
+    await createTransaction(transaction)
+    closeBottomSheet()
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      const errors = {} as ValidationErrorsTypes
+
+      error.inner.forEach((err) => {
+        if (err.path) {
+          errors[err.path as keyof CreateTransactionRequest] = err.message
+        }
+      })
+
+      setValidationErrors(errors)
+    } else {
+      errorHandler(error,'Falha ao criar transação')
+    }
+  } finally {
+    setLoading(false)
+  }
+}
+  return (
+    <View className="px-8">
+      <View className="w-full flex-row items-center justify-between">
+        <Text className="text-white text-xl font-bold">
           Nova transação
-</Text>
-
-<TouchableOpacity onPress={closeBottomSheet}>
-<MaterialIcons
-name="close"
-color={colors.gray[700]}
-size={20}
-/>
-</TouchableOpacity>
-</View>
-
-<View className="flex-1 mt-8 mb-8">
-<TextInput
-className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
-placeholder="Descrição"
-placeholderTextColor={colors.gray[700]}
-value={transaction.description}
-onChangeText={(text) =>setTransactionData('description',text)}
-/>
-
-<CurrencyInput
-className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
-value={transaction.value}
-prefix="R$ "
-delimiter="."
-separator=","
-precision={2}
-minValue={0}
-onChangeValue={(value) =>setTransactionData('value',value??0)}
-/>
-
-<SelectCategoryModal
-selectedCategory={transaction.categoryId}
-onSelect={(categoryId) =>setTransactionData('categoryId',categoryId)}
-/>
-
-<TransactionTypeSelector
-typeId={transaction.typeId}
-setTransactionType={(typeId) =>setTransactionData('typeId',typeId)}
-/>
-</View>
-</View>
+        </Text>
+        <TouchableOpacity onPress={closeBottomSheet}>
+          <MaterialIcons
+            name="close"
+            color={colors.gray[700]}
+            size={20}
+          />
+        </TouchableOpacity>
+      </View>
+      <View className="flex-1 mt-8 mb-8">
+        <TextInput
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
+          placeholder="Descrição"
+          placeholderTextColor={colors.gray[700]}
+          value={transaction.description}
+          onChangeText={(text) => setTransactionData('description', text)}
+        />
+        {validationErrors.description && (
+          <ErrorMessage>
+            {validationErrors.description}
+          </ErrorMessage>
+        )}
+        <CurrencyInput
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-md pl-4"
+          value={transaction.value}
+          prefix="R$ "
+          delimiter="."
+          separator=","
+          precision={2}
+          minValue={0}
+          onChangeValue={(value) => setTransactionData('value', value ?? 0)}
+        />
+        {validationErrors.value && (
+          <ErrorMessage>
+            {validationErrors.value}
+          </ErrorMessage>
+        )}
+        <SelectCategoryModal
+          selectedCategory={transaction.categoryId}
+          onSelect={(categoryId) => setTransactionData('categoryId', categoryId)}
+        />
+        {validationErrors.categoryId && (
+          <ErrorMessage>
+            {validationErrors.categoryId}
+          </ErrorMessage>
+        )}
+        <TransactionTypeSelector
+          typeId={transaction.typeId}
+          setTransactionType={(typeId) => setTransactionData('typeId', typeId)}
+        />
+        {validationErrors.typeId && (
+          <ErrorMessage>
+            {validationErrors.typeId}
+          </ErrorMessage>
+        )}
+        <View>
+          <AppButton onPress={handleCreateTransaction}>
+            {loading ? (
+              <ActivityIndicator color={colors.white}/>
+            ) : (
+              'Registrar'
+            )}
+          </AppButton>
+        </View>
+      </View>
+    </View>
   )
 }
--- src/context/transaction.context.tsx
+++ src/context/transaction.context.tsx
@@ -1,46 +1,53 @@
-import {TransactionCategory }from'@/shared/interfaces/http/transaction-category-response'
-import * as TransactionService from'@/shared/services/dtMoney/transaction.service'
+import { CreateTransactionRequest } from '@/shared/interfaces/http/createTransactionRequest'
+import { TransactionCategory } from '@/shared/interfaces/http/transaction-category-response'
+import * as TransactionService from '@/shared/services/dtMoney/transaction.service'
 import {
-FC,
-PropsWithChildren,
-createContext,
-useContext,
-useState,
-}from'react'
+  FC,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+} from 'react'
 
-type TransactionContextType= {
-  fetchCategories: () =>Promise<void>
-  categories:TransactionCategory[]
+type TransactionContextType = {
+  fetchCategories: () => Promise<void>
+  createTransaction: (transaction: CreateTransactionRequest) => Promise<void>
+  categories: TransactionCategory[]
 }
 
-export const TransactionContext=createContext({} as TransactionContextType)
+export const TransactionContext = createContext({} as TransactionContextType)
 
-export const TransactionContextProvider:FC<PropsWithChildren>= ({
+export const TransactionContextProvider: FC<PropsWithChildren> = ({
   children,
 }) => {
-const [categories,setCategories]=useState<TransactionCategory[]>([])
+  const [categories, setCategories] = useState<TransactionCategory[]>([])
 
-const fetchCategories=async () => {
-const categoriesResponse=
-await TransactionService.getTransactionCategories()
-
-setCategories(categoriesResponse)
+  const fetchCategories = async () => {
+    const categoriesResponse =
+      await TransactionService.getTransactionCategories()
+    setCategories(categoriesResponse)
   }
 
-return (
-<TransactionContext.Provider
-value={{
+  const createTransaction = async (
+    transaction: CreateTransactionRequest
+  ) => {
+    await TransactionService.createTransaction(transaction)
+  }
+
+  return (
+    <TransactionContext.Provider
+      value={{
         categories,
         fetchCategories,
+        createTransaction,
       }}
->
+    >
       {children}
-</TransactionContext.Provider>
+    </TransactionContext.Provider>
   )
 }
 
-export const useTransactionContext= () => {
-const context=useContext(TransactionContext)
-
-return context
+export const useTransactionContext = () => {
+  const context = useContext(TransactionContext)
+  return context
 }
