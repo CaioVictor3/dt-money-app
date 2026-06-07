962c7ee2e5a6c8257e07173d0486dd17155eac30
Caio <caiovictorarantes@gmai.com>
2026-06-07 11:54:30 -0300

    contexto importa services de auth

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import NavigationRoutes from '@/routes'
-import './src/styles/global.css'
+import'./src/styles/global.css'
+
+import {AuthContextProvider }from'@/context/auth.context'
+
 
 export default function App() {
-  return <NavigationRoutes />
+return (
+<AuthContextProvider>
+<NavigationRoutes/>
+</AuthContextProvider>
+  )
 }
